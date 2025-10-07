6b3d9fffea4bbf88197dbd3438b24cf7f86397c1
fadilsyy <fadilasytar@gmail.com>
2025-10-07 14:56:58 +0700

    compleated

--- file1.py
+++ file1.py
@@ -2,3 +2,11 @@ percentage = float(input("Masukkan persentase nilai siswa: "))
 
 if percentage >= 90:
     print("Excellent performance")
+elif percentage >= 80:
+    print("Very Good Performance")
+elif percentage >= 70:
+    print("Good Performance")
+elif percentage >= 60:
+    print("Average Performance")
+else:
+    print("Needs Improvement")
